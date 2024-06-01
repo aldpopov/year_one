982b6364ac6f16a6d53b96f1f9cf3652a431341f
aldpopov <mercenarydaim@gmail.com>
2024-06-02 01:54:11 +0300

    Report_24 template

---
+++ LW_24/header.h
@@ -0,0 +1,9 @@
+typedef struct _node {
+    struct _node* left;
+    struct _node* right;
+    char* value;
+} Node;
+
+Node* parseExpression(char** expression);
+Node* parseTerm(char** expression);
+Node* parseFactor(char** expression);
